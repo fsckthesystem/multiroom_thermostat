fdd24a3aa7c734f1677e14821709ca73bbd68cbd
fsckthesystem <disturbedmaggot@me.com>
2021-03-05 20:17:50 -0700

    Dynamically handles nodes instead of hard coding
    notifies if not receiving from any nodes

--- thermostat_server/thermostat_server.py
+++ thermostat_server/thermostat_server.py
@@ -5,6 +5,7 @@ import threading
 import math
 import datetime
 from collections import deque
+from itertools import combinations
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM) # set pin naming scheme to rpi
@@ -28,7 +29,7 @@ RELAYOFF = GPIO.HIGH
 RELAYON = GPIO.LOW
 
 # Tempurature range
-TEMPHIGH = 78
+TEMPHIGH = 76
 TEMPLOW = 69
 
 # Using deques as they can have a fixed number and drop off the oldest
@@ -46,18 +47,23 @@ rolling_hum_living_room = deque(DEQUELENGTH*[40], DEQUELENGTH)
 rolling_temp_office = deque(DEQUELENGTH*[(TEMPHIGH + TEMPLOW) / 2], DEQUELENGTH)
 rolling_hum_office = deque(DEQUELENGTH*[40], DEQUELENGTH)
 
+rolling_temps = {}
+rolling_hums = {}
+
+total_nodes = 0
+
 lr_last_received = datetime.datetime.utcnow()
 off_last_received = datetime.datetime.utcnow()
-
+last_received = {}
 
 def main():
     # Creating thread for receiving climate info from sensor nodes
-    collecting_thread = threading.Thread(target=data_collection)
+    collecting_thread = threading.Thread(target=data_collection_wd)
     collecting_thread.daemon = True
 
     # Creating thread for checking if nodes have sent data in given 
     # amount of time.
-    checking_thread = threading.Thread(target=node_check)
+    checking_thread = threading.Thread(target=node_check_wd)
     checking_thread.daemon = True
 
     # Start up server
@@ -65,7 +71,7 @@ def main():
         init()
         collecting_thread.start()
         checking_thread.start()
-        run()
+        run_wd()
 
     # Catch ctrl-c and cleanly exit program
     except KeyboardInterrupt:
@@ -77,7 +83,6 @@ def main():
 
 # collects incoming data from nodes and sorts into correct deque
 def data_collection():
-
     global lr_last_received
     global off_last_received
     while True:
@@ -93,7 +98,26 @@ def data_collection():
             off_last_received = datetime.datetime.utcnow()
             rolling_temp_office.appendleft(temp)
             rolling_hum_office.appendleft(hum)
-       
+
+
+
+def data_collection_wd():
+    while True:
+        data, addr = SOCK.recvfrom(1024) # buffer size is 1024 bytes
+        message = data.decode()
+        loc, temp, hum = message.split(", ")
+        temp = (float(temp) * 1.8) + 32 # convert from C to F
+
+        if loc not in rolling_temps:
+            rolling_temps[loc] = deque(DEQUELENGTH*[((TEMPHIGH + TEMPLOW)/2)], DEQUELENGTH)
+        if loc not in rolling_hums:
+            rolling_hums[loc] = deque(DEQUELENGTH*[40], DEQUELENGTH)
+        
+        rolling_temps[loc].appendleft(temp)
+        rolling_hums[loc].appendleft(hum)
+        last_received[loc] = datetime.datetime.utcnow()
+
+
 
 def node_check():       
         # check if the nodes have stopped sending data; if one has, set data to the average
@@ -111,6 +135,23 @@ def node_check():
             print("Lost contact with Office")
             rolling_temp_office = deque(DEQUELENGTH*[((TEMPHIGH + TEMPLOW)/2)], DEQUELENGTH)
             rolling_hum_office = deque(DEQUELENGTH*[40], DEQUELENGTH)
+        time.sleep(60)
+
+
+def node_check_wd():       
+        # check if the nodes have stopped sending data; if one has, set data to the average
+        # TEMPHIGH and TEMPLOW, and humidity to 40%
+    global total_nodes
+    while True:
+        if len(last_received) > 0:
+            for loc in last_received:
+                if (datetime.datetime.utcnow() - last_received[loc]).total_seconds() >= 600:
+                    rolling_temps.pop(loc)
+                    rolling_hums.pop(loc)
+                    last_received.pop(loc)
+                else:
+                    print("No nodes found. Please check that nodes are running.")
+
         time.sleep(60)
 
 
@@ -151,19 +192,61 @@ def run():
             time.sleep(120)
 
 
-def prep():
-    print("Populating buffers")
-    while None in rolling_temp_living_room or None in rolling_temp_office:
-        data, addr = SOCK.recvfrom(1024) # buffer size is 1024 bytes
-        message = data.decode()
-        loc, temp, hum = message.split(", ")
-        temp = (float(temp) * 1.8) + 32
-        if loc == "Living Room":
-            rolling_temp_living_room.appendleft(temp)
-            rolling_hum_living_room.appendleft(hum)
-        if loc == "Office":
-            rolling_temp_office.appendleft(temp)
-            rolling_hum_office.appendleft(hum)
+
+def run_wd():
+    time.sleep(7)
+    print("Starting thermostat")
+    
+    # loop to check rolling average of room temps and to set systems accordingly
+    while True:
+
+        loc_temp_avg = {}
+        above_temphigh = False
+        below_templow = False
+        loc_temp_diff = 0
+        
+        if len(rolling_temps) > 0:
+            for loc in rolling_temps:
+                loc_temp_avg[loc] = sum(rolling_temps[loc])/len(rolling_temps[loc])
+                if loc_temp_avg[loc] > TEMPHIGH:
+                    above_temphigh = True
+                elif loc_temp_avg[loc] < TEMPLOW:
+                    below_templow = True
+            if len(rolling_temps) > 1:
+                loc_temp_diff_tuple = max(combinations(loc_temp_avg, 2), key = lambda temps: abs(loc_temp_avg[temps[0]] - loc_temp_avg[temps[1]]))
+                loc_temp_diff = loc_temp_avg[loc_temp_diff_tuple[1]] - loc_temp_avg[loc_temp_diff_tuple[0]]
+                print(loc_temp_diff)
+        else:
+            continue
+
+        if loc_temp_diff > 3:
+            print("Temps vary too much; toggling fan on")
+            GPIO.output(HEATPIN, RELAYOFF)
+            GPIO.output(COOLPIN, RELAYOFF)
+            GPIO.output(FANPIN, RELAYON)
+            time.sleep(300)
+
+        elif below_templow:
+            print("Temp is low; toggling heat on")
+            GPIO.output(COOLPIN, RELAYOFF)
+            GPIO.output(FANPIN, RELAYON)
+            GPIO.output(HEATPIN, RELAYON)
+            time.sleep(300)
+
+        elif above_temphigh:
+            print("Temp is high; toggling cooling on")
+            GPIO.output(HEATPIN, RELAYOFF)
+            GPIO.output(FANPIN, RELAYON)
+            GPIO.output(COOLPIN, RELAYON)
+            time.sleep(300)
+        
+        else:
+            print("Climate is within set parameters; toggling systems off if any are on")
+            GPIO.output(HEATPIN, RELAYOFF)
+            GPIO.output(COOLPIN, RELAYOFF)
+            GPIO.output(FANPIN, RELAYOFF)
+            time.sleep(120)
+
 
 
 def init():
