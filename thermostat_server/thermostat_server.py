06d98ad74de645905247e6a417d985536fe06814
fsckthesystem <disturbedmaggot@me.com>
2021-03-07 06:53:33 -0700

    updated and improved comments in thermostat_server.py

--- thermostat_server/thermostat_server.py
+++ thermostat_server/thermostat_server.py
@@ -18,6 +18,7 @@ SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 SOCK.bind((UDP_IP, UDP_PORT))
 
 # set up pins
+## Change these pins to match what you are using
 FANPIN = 22
 HEATPIN = 17
 COOLPIN = 10
@@ -32,20 +33,22 @@ RELAYON = GPIO.LOW
 TEMPHIGH = 76
 TEMPLOW = 69
 
+# rolling_X variables are populated in the data_collection function with
+# deques dynamically for each node that receives from.
+
 # Using deques as they can have a fixed number and drop off the oldest
 # when a new value is added.
 # This gives make getting a rolling average easier
 
-### TODO: Make a dictionary that use the room name as the key and the value being a deque.
-### This will make it easier to add new rooms dynamically instead of having to hard
-### program it in.
-
-DEQUELENGTH = 20 # size of the deques
 
 rolling_temps = {}
 rolling_hums = {}
 
+DEQUELENGTH = 20 # size of the deques
+
+# Keeps track of time of last received datagram for each location
 last_received = {}
+
 
 def main():
     # Creating thread for receiving climate info from sensor nodes
@@ -80,19 +83,22 @@ def data_collection():
         loc, temp, hum = message.split(", ")
         temp = (float(temp) * 1.8) + 32 # convert from C to F
 
+        # Checks if location is alreay in the rolling_X dictionarys. If not, it creates an entry
+        # in the dictionary and populates it with the defaults
         if loc not in rolling_temps:
             rolling_temps[loc] = deque(DEQUELENGTH*[((TEMPHIGH + TEMPLOW)/2)], DEQUELENGTH)
         if loc not in rolling_hums:
             rolling_hums[loc] = deque(DEQUELENGTH*[40], DEQUELENGTH)
         
+        # Append new temp and humidity to appropriate deque in dictionaries
         rolling_temps[loc].appendleft(temp)
         rolling_hums[loc].appendleft(hum)
         last_received[loc] = datetime.datetime.utcnow()
 
 
+# Checks if any location has sent data in the past 10 mins
+# if it has not, it removes location from dictionaries
 def node_check():       
-        # check if the nodes have stopped sending data; if one has, set data to the average
-        # TEMPHIGH and TEMPLOW, and humidity to 40%
     time.sleep(7)
     while True:
         if len(last_received) > 0:
@@ -118,6 +124,9 @@ def run():
         below_templow = False
         loc_temp_diff = 0
         
+        # Logic for determining average temp for location, temp differences, and 
+        # checks for temps below or above set TEMPMAX and TEMPLOW and sets the 
+        # above variables accordingly
         if len(rolling_temps) > 0:
             for loc in rolling_temps:
                 loc_temp_avg[loc] = sum(rolling_temps[loc])/len(rolling_temps[loc])
@@ -129,6 +138,7 @@ def run():
                 loc_temp_diff_tuple = max(combinations(loc_temp_avg, 2), key = lambda temps: abs(loc_temp_avg[temps[0]] - loc_temp_avg[temps[1]]))
                 loc_temp_diff = loc_temp_avg[loc_temp_diff_tuple[1]] - loc_temp_avg[loc_temp_diff_tuple[0]]
 
+        # Toggles fan on if difference in temps is too much
         if loc_temp_diff > 3:
             print("Temps vary too much; toggling fan on")
             GPIO.output(HEATPIN, RELAYOFF)
@@ -136,6 +146,7 @@ def run():
             GPIO.output(FANPIN, RELAYON)
             time.sleep(300)
 
+        # Toggles on heat if temp average is too low
         elif below_templow:
             print("Temp is low; toggling heat on")
             GPIO.output(COOLPIN, RELAYOFF)
@@ -143,6 +154,7 @@ def run():
             GPIO.output(HEATPIN, RELAYON)
             time.sleep(300)
 
+        # Toggles on AC if temp average is too high
         elif above_temphigh:
             print("Temp is high; toggling cooling on")
             GPIO.output(HEATPIN, RELAYOFF)
@@ -150,6 +162,7 @@ def run():
             GPIO.output(COOLPIN, RELAYON)
             time.sleep(300)
         
+        # Toggles everything off if disired conditions are met
         else:
             print("Climate is within set parameters; toggling systems off if any are on")
             GPIO.output(HEATPIN, RELAYOFF)
