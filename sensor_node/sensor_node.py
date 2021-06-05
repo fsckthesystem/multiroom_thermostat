7cc8472f1d4f341908774f04984ff31d32b96721
Preston Haynes <disturbedmaggot@me.com>
2021-06-05 08:56:43 -0600

    updated sensor_node.py to use adafruit_circuitpython_dht as adafruit_python_dht has been deprecated

--- sensor_node/sensor_node.py
+++ sensor_node/sensor_node.py
@@ -1,7 +1,8 @@
 #
 # This code is originally by Mike Murray (@TheGeekPub) from https://www.thegeekpub.com/236867/using-the-dht11-temperature-sensor-with-the-raspberry-pi/
 #
-# I have slightly modified it to meet the requirements of my project.
+# I have slightly modified it to meet the requirements of my project and updated it to use the new adafruit-circuitpython-dht library as the
+# adafruit-python-dht library has been deprecated.
 #
 
 
@@ -9,7 +10,8 @@
 import time
 import socket
 import sys
-import Adafruit_DHT
+import board
+import adafruit_dht
 
 
 if len(sys.argv) != 3:
@@ -20,15 +22,16 @@ ROOM = sys.argv[1]
 SERVER_HOSTNAME = sys.argv[2]
 UDP_IP = socket.gethostbyname(SERVER_HOSTNAME)
 UDP_PORT = 4815
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
+DHT_PIN = board.D4
+DHT_SENSOR = adafruit_dht.DHT22(DHT_PIN)
 
 
 SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
 while True:
-    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    temperature = DHT_SENSOR.temperature
+    humidity = DHT_SENSOR.humidity
     if humidity is not None and temperature is not None:
         print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
         message = str.encode("{0}, {1:0.1f}, {2:0.1f}".format(ROOM, temperature, humidity))
